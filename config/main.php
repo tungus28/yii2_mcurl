0e65200cc8150e4ef9cba120587b2d45f4b7297c
tungus28 <tungus28@gmail.com>
2015-09-14 22:01:02 +0400

    first commit

---
+++ config/main.php
@@ -0,0 +1,57 @@
+<?php
+$params = array_merge(
+    require(__DIR__ . '/../../common/config/params.php'),
+    require(__DIR__ . '/../../common/config/params-local.php'),
+    require(__DIR__ . '/params.php'),
+    require(__DIR__ . '/params-local.php')
+);
+
+return [
+    'id' => 'app-frontend',
+    'basePath' => dirname(__DIR__),
+    'bootstrap' => ['log'],
+    'controllerNamespace' => 'frontend\controllers',
+	'modules' => [
+		'gii' => [
+			'class' => 'yii\gii\Module',
+			'allowedIPs' => ['*'],
+		],
+		'webshell' => [
+			'class' => 'samdark\webshell\Module',
+			'allowedIPs' => ['*']
+		],
+    ],	
+    'components' => [
+        'user' => [
+            'identityClass' => 'common\models\User',
+            'enableAutoLogin' => true,
+        ],
+        'log' => [
+            'traceLevel' => YII_DEBUG ? 3 : 0,
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
+        'errorHandler' => [
+            'errorAction' => 'site/error',
+        ],
+		'view' => [
+            'renderers' => [
+                'twig' => [
+                    'class' => 'yii\twig\ViewRenderer',
+                    // set cachePath to false in order to disable template caching
+                    'cachePath' => '@runtime/Twig/cache',
+                    // Array of twig options:
+                    'options' => [
+                        'auto_reload' => true,
+                    ],
+                    // ... see ViewRenderer for more options
+                ],
+            ],
+        ],
+    ],
+    'params' => $params,
+];
