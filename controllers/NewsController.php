0408afcfa4d21321c30092a76875bd1d3996f660
tungus28 <tungus28@gmail.com>
2015-09-19 19:49:36 +0400

    added jquery loading in head

--- controllers/NewsController.php
+++ controllers/NewsController.php
@@ -2,11 +2,14 @@
 
 namespace frontend\controllers;
 
-class NewsController extends \yii\web\Controller
+use yii\web\Controller;
+use yii\web\View;
+
+class NewsController extends Controller
 {
     public function actionIndex()
     {
-        //$this->view->registerJsFile('js/jquery-min.js');
+        //$this->view->registerJsFile('js/jquery-min.js', ['position' => View::POS_HEAD]);
         //$this->view->registerCssFile('css/bootstrap.min.css');
 
         return $this->render('index.twig', []);
